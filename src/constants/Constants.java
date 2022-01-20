67dc72deec426c4670bbce9c73137bf44fee3423
Richard Noteboom <richard@noteboom.org>
2022-01-19 19:08:44 -0500

    This is a recreation of the dice game Yahtzee using Java.

---
+++ src/constants/Constants.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constants;
+
+/**
+ *
+ * @author Richard Noteboom
+ */
+public class Constants {
+    public final static int MAX_YAHTZEE = 4;
+    public final static int NUM_DICE = 5;
+    public final static int MAX_DIE_VALUE = 6;    
+    public final static int ONES = 1; // category number
+    public final static int TWOS = 2; // category number
+    public final static int THREES = 3; // category number
+    public final static int FOURS = 4; // category number
+    public final static int FIVES = 5; // category number
+    public final static int SIXES = 6; // category number
+    public final static int THREE_KIND = 7; // category number
+    public final static int FOUR_KIND = 8; // category number
+    public final static int CHANCE = 9; // category number
+    public final static int NUM_CATERGORY = 13;
+    public final static int FULL_HOUSE = 25; // category points
+    public final static int SM_STRAIGHT = 30; // category points
+    public final static int UPPER_BONUS = 35; // category points
+    public final static int LG_STRAIGHT = 40; // category points
+    public final static int YAHTZEE = 50; // category points
+    public final static int YAHTZEE_BONUS = 100; // category points
+    public final static int ZERO = 0;
+    public final static int MAX_ROLLS = 3;
+}
